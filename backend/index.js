1b89643f92ba3202f6d8f83b7036067f2001e64d
Sunny Wu <w.sunny0618@gmail.com>
2025-09-26 22:50:32 -0400

    signup and login endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,29 @@
-// import express
 const express = require('express');
-
-// import dotenv
+const app = express();
+app.use(express.json());
 require('dotenv').config();
 
 // connect to mongoDB
 const connectDB = require('./config/db');
 connectDB();
 
-// use middleware
-const app = express();
-app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
 
-// ROUTES
+app.get('/', (req, res) => {
+  res.send('Server is alive!');
+});
+
+// import routes
+const authRoutes = require('./routes/auth');
+
+// mount routes
+app.use('/api/auth', authRoutes);
 
 // start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
