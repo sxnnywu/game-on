52f1982d06c82fa95d5ce8c58b1db175b2000282
Elizabeth Wright <e3wright@uwaterloo.ca>
2025-09-28 03:59:42 -0400

    attempt 4 for my team functionality

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -28,7 +28,7 @@ router.post('/signup', async (req, res) => {
         });
 
         await newUser.save();
-        res.status(201).json({ message: 'User created successfully' }, {user: newUser});
+        res.status(201).json({ message: 'User created successfully' }, {userID: newUser.id});
     } 
     catch (err) {
         console.error(err);
@@ -57,7 +57,7 @@ router.post('/login', async (req, res) => {
             process.env.JWT_SECRET,
             { expiresIn: '1d' }
         );
-        res.json({ token }, {user: user});
+        res.json({ token }, {userID: newUser.id});
     } 
     catch (err) {
         console.error(err);
--- frontend/src/Components/Auth/Auth.js
+++ frontend/src/Components/Auth/Auth.js
@@ -63,7 +63,7 @@ const Auth = () => {
       }
       setError('');
       localStorage.setItem('token', data.token);
-      localStorage.setItem('userId', data.user.id)
+      localStorage.setItem('userId', data.userID)
       navigate('/dashboard');
     } catch (error) {
       console.error("Network error:", error);
